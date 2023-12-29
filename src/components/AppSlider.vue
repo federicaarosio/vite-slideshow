aa4dec7c573583cb1bddaa2a5282b0b4d49ee759
federicaarosio <arosio.fede@gmail.com>
2023-12-29 12:14:25 +0100

    working on api

--- src/components/AppSlider.vue
+++ src/components/AppSlider.vue
@@ -12,8 +12,33 @@ export default {
 
     data() {
         return {
-            
+            moviesList: [],
         }
+    },
+
+    methods: {
+
+        getMovieDetails() {
+            // CHIAMATA AXIOS per ottenere i dettagli del film
+            axios.get('https://api.themoviedb.org/3/movie/', {
+                params: {
+                    movie_id: "547205",
+                    api_key: "035eedbe7389d3160c097087910908d0",
+                }
+            })
+            .then((response) => {
+                console.log(response.data);
+                this.moviesList.push(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }  
+    },
+
+    created() {
+        console.log(this.moviesList);
+        this.getMovieDetails
     },
 }
 </script>
