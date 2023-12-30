b297478673af19f80e01c288c926f0bc7c9fb2f8
federicaarosio <arosio.fede@gmail.com>
2023-12-30 18:18:11 +0100

    removed AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template lang="">
-  <AppHeader />
   <AppMain />
 </template>
 
 <script>
-import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
 export default {
   components: {
-    AppHeader,
     AppMain
   },
 
